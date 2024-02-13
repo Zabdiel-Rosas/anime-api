8c913857b6c6009c665a29799f2858e96c22c23e
okami <zabdiel_rosas@outlook.com>
2024-02-13 16:17:46 -0600

    feat(animeServices): crud services are created

--- src/services/animeServices.ts
+++ src/services/animeServices.ts
@@ -1,6 +1,43 @@
-import { Anime } from '../types'
+import { Anime, NewAnime } from '../types'
 import animeData from './data.json'
 
 const animes: Anime[] = animeData as Anime[]
 
-export const getAnimes = () => animes
+const getAnimes = () => animes
+
+const addAnime = (newAnimeData: NewAnime): Anime => {
+  const newAnime = {
+    id: Math.max(...animes.map((a) => a.id)) + 1,
+    ...newAnimeData
+  }
+
+  animes.push(newAnime)
+  return newAnime
+}
+
+const getOneAnime = (id: number): Anime | undefined => {
+  const anime = animes.find((a) => a.id === id)
+  return anime
+}
+
+const modifyAnime = (id: number, newData: Anime): Anime | undefined => {
+  const index = animes.findIndex((a) => a.id === id)
+
+  if (index !== -1) {
+    const updatedAnime = { ...animes[index], ...newData }
+    animes.splice(index, 1, updatedAnime)
+
+    return updatedAnime
+  }
+}
+
+const eraseAnime = (id: number): Anime | undefined => {
+  const index = animes.findIndex((a) => a.id === id)
+
+  if (index !== -1) {
+    const deletedAnime = animes.splice(index, 1)
+    return deletedAnime[0]
+  }
+}
+
+export { getAnimes, addAnime, modifyAnime, getOneAnime, eraseAnime }
